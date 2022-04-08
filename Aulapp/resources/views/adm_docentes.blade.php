723fd6a4bed633dcba1d42ef31b425f04d2589b4
emmanuelmamani <emmanuelmamani100@gmail.com>
2022-04-07 20:50:14 -0400

    Plantilla Administrador

---
+++ Aulapp/resources/views/adm_docentes.blade.php
@@ -0,0 +1,11 @@
+@extends('administrador')
+@section('Titulo')
+<h3 text-center>Administracion de doncentes</h3>
+@endsection
+@section('Titulo formulario')
+<h3 text-center>Registrar doncente</h3>
+@endsection
+@section('Contenido formulario')
+<label for="inputNombre" class="form-label">Nombre</label>
+<input type="text" id="inputNombre" class="form-control">
+@endsection
--- Aulapp/routes/web.php
+++ Aulapp/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/adm', function () {
+    return view('adm_docentes');
+});
