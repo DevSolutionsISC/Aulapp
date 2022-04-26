fef7c36be71d3be655991a6b9d0a7ab6a72ccbbf
Mariela-Fernandez297 <fernandezrealmariela@gmail.com>
2022-04-26 18:44:53 -0400

    Reporte grupo y docente

---
+++ Aulapp/resources/views/reporte_docente.blade.php
@@ -0,0 +1,42 @@
+@extends('plantilla')
+@section('title', 'Seccion')
+@section('Titulo')
+<h3 text-center>Administracion de docentes </h3>
+@endsection
+@section('Contenido formulario')
+@section('Tabla')
+<div id="C_tabla">
+      <h3 id="T_tabla">Lista de docentes</h3>
+      <table class="table">
+      
+            <thead>                
+                  <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">CI</th>
+                  </tr>
+            </thead>
+            <tbody>
+                    @foreach($usuarios as $usuario)
+                   <tr>
+                         <td>{{$usuario->Nombre}}</td>
+                         <td>{{$usuario->Apellido}}</td>
+                         <td>{{$usuario->CI}}</td>
+                         
+                         <td>
+                               <div><a href="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                            class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    </svg>
+                               </a>
+                               </div>
+                             
+                         </td>
+                         
+                   </tr>   
+                   @endforeach  
+                  
+            </tbody>
+      </table>
+</div>
+@endsection
