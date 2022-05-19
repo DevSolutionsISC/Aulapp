5df663963a8b427cf03305e5315a9d33a35e4b37
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2022-05-18 20:42:38 -0400

    disponibilidad de hora y fecha

--- Aulapp/resources/views/respuesta.blade.php
+++ Aulapp/resources/views/respuesta.blade.php
@@ -64,6 +64,7 @@
       use App\Models\AulaAsignada;
       $aulas_asignadas=AulaAsignada::all();
 
+
       @endphp
       <div class="accordion accordion-flush" id="accordionFlushExample">
         @foreach ($sections as $section)
@@ -112,9 +113,11 @@
                 var aula = document.getElementById('{{$aula->id}}');
                 if ('{{$aula->section_id}}'=='{{$section->id}}'){
                 @foreach ($aulasAsignadas as $aulaAsignada)
-                  if({{$aula->id}}=={{$aulaAsignada->aula_id}}){
+                  if({{$aula->id}}=={{$aulaAsignada->aula_id}} && (('{{$aulaAsignada->reserva->fecha_examen}}'== '{{$reserva->fecha_examen}}') && ('{{$aulaAsignada->reserva->hora_inicio}}' < '{{$aulaAsignada->reserva->hora_fin}}' && '{{$reserva->hora_inicio}}' < '{{$reserva->hora_fin}}'  ) ) ){
                     aula.style.display = 'none';
                     suma = suma - {{$aula->capacidad}};
+                    console.log('aula asignada para la fecha de:'+'{{$aulaAsignada->reserva->fecha_examen}}');
+                   console.log('fecha de aula por asignar: '+ '{{$reserva->fecha_examen}}');
                   }else{
                     suma = suma + {{$aula->capacidad}};
                     aula.style.display = 'block';
