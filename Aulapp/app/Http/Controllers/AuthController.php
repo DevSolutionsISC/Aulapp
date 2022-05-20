826405d339e6b489cf4ea836d83d741a80816c45
Mariela-Fernandez297 <fernandezrealmariela@gmail.com>
2022-05-19 20:28:18 -0400

    Validaciones y funcion mostrar/ocultar contraseña

--- Aulapp/app/Http/Controllers/AuthController.php
+++ Aulapp/app/Http/Controllers/AuthController.php
@@ -6,35 +6,55 @@ use App\Models\Usuario;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreLogin;
 class AuthController extends Controller
 {
     /**
      * @param Request $request
      * @return RedirectResponse
      */
-    public function authenticate(Request $request): RedirectResponse
+    public function authenticate(StoreLogin $request): RedirectResponse
     {
         $credentials = $request->validate([
             'usuario' => ['required'],
             'contrasenia' => ['required'],
         ]);
 
+
         $usuario = Usuario::query()
             ->where('usuario', $credentials['usuario'])
             ->where('contrasenia', $credentials['contrasenia'])
             ->first();
+
+      
 
         if ($usuario) {
             Auth::login($usuario);
             $request->session()->regenerate();
             return redirect()->intended('/menu')->with('id',$usuario->id);
         }
+         else{
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'usuario' => __('auth.failed'),
+                'contrasenia' => __('auth.failed')
+                
+            ]);
+         }     
+      
 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
+       /* if('contrasenia'!=$credentials['contrasenia'])
+        {
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'contrasenia' => __('auth.password'),
+                
+            ]);
+        }*/
+
     }
+
+
 
     /**
      * @param Request $request
--- Aulapp/resources/views/respuesta.blade.php
+++ Aulapp/resources/views/respuesta.blade.php
@@ -34,7 +34,7 @@
             <a href="bandeja_administrador"><span class="material-symbols-outlined" id="campana">
                 notifications
                 </span></a>
-          <a class="nav-link active" aria-current="page" href="menu_adm" id="inicio">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="menu" id="inicio">Inicio</a>
           <a class="nav-link active" aria-current="page" href="@yield("registrar")" id="registrar">DevSolution</a>
         </form>
       </div>
