b3e292168b6c43abd13f4ea740fdd0b53836a610
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2022-06-19 16:11:00 -0400

    Cambiando section a seccion en models y controllers

--- Aulapp/app/Models/Aula.php
+++ Aulapp/app/Models/Aula.php
@@ -3,22 +3,21 @@
 namespace App\Models;
 
 use App\Models\AulaAsignada;
-use App\Models\Section;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Aula extends Model
 {
-    use HasFactory;
-    protected $table = 'aulas';
-    protected $primaryKey = 'id';
-    public function section()
-    {
-        return $this->belongsTo(Section::class, 'section_id');
-    }
-    public function aula_asignada()
-    {
-        return $this->hasMany(AulaAsignada::class, 'aula_id');
-    }
+ use HasFactory;
+ protected $table      = 'aulas';
+ protected $primaryKey = 'id';
+ public function seccion()
+ {
+  return $this->belongsTo(Seccion::class, 'section_id');
+ }
+ public function aula_asignada()
+ {
+  return $this->hasMany(AulaAsignada::class, 'aula_id');
+ }
 
 }
